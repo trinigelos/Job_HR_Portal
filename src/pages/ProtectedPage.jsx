3760a47e2d8a0f2f313856046a911cf7ad8fdba2
trinigelos <trinidadgelos7@gmail.com>
2024-11-11 21:03:31 +0100

    updates frontend

--- src/pages/ProtectedPage.jsx
+++ src/pages/ProtectedPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Sidebar from "./Protected/SideBar";
 import "./Protected/Dashboard.css"
 import SearchBar from "../components/SearchBar/SearchBar";
